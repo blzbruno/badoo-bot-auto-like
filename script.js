1bd57e4704f141adf76ab36ff15ae5251e9566d9
Bruno Luiz <brunoluizdepaula@yahoo.com.br>
2018-06-13 03:33:07 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function cclick(){	
-	document.getElementsByClassName("b-link js-profile-header-vote")[0].click();
+	document.getElementsByClassName("js-profile-header-vote-yes")[0].click();
 }
 
 var myVarc = setInterval(function(){ cclick() }, 100);
